ef66bf2eb928dde969554e1b34b7eea978fa8e81
aavi4012-cmd <heyitzavinash@gmail.com>
2025-04-02 14:55:05 +0530

    deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/component/home/Header"
-
+import Footer from "@/component/Footer"
 export default function Home() {
   return (
     <>
       <main className="space-y-8 overflow-x-hidden">
         <Header />
+
+        <Footer/>
       </main>
     </>
     
